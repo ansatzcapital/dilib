08fcd16d9c18afcb94f83e35f388384b817481e1
yanovs <42386943+yanovs@users.noreply.github.com>
2023-05-14 19:48:23 -0400

    Export public API properly (#7)
    * Export public API properly (via redundant imports)
    
    * Update README examples

--- dilib/__init__.py
+++ dilib/__init__.py
@@ -1,25 +1,24 @@
 # flake8: noqa
-from dilib.config import Config, get_config
-from dilib.container import ConfigProxy, Container, get_container
-from dilib.errors import (
-    ConfigError,
-    FrozenConfigError,
-    InputConfigError,
-    NewKeyConfigError,
-    SetChildConfigError,
-)
-from dilib.specs import (
-    Forward,
-    GlobalInput,
-    LocalInput,
-    Object,
-    Prototype,
-    PrototypeMixin,
-    Singleton,
-    SingletonDict,
-    SingletonList,
-    SingletonMixin,
-    SingletonTuple,
-    Spec,
-    SpecID,
-)
+from dilib.config import Config as Config
+from dilib.config import get_config as get_config
+from dilib.container import ConfigProxy as ConfigProxy
+from dilib.container import Container as Container
+from dilib.container import get_container as get_container
+from dilib.errors import ConfigError as ConfigError
+from dilib.errors import FrozenConfigError as FrozenConfigError
+from dilib.errors import InputConfigError as InputConfigError
+from dilib.errors import NewKeyConfigError as NewKeyConfigError
+from dilib.errors import SetChildConfigError as SetChildConfigError
+from dilib.specs import Forward as Forward
+from dilib.specs import GlobalInput as GlobalInput
+from dilib.specs import LocalInput as LocalInput
+from dilib.specs import Object as Object
+from dilib.specs import Prototype as Prototype
+from dilib.specs import PrototypeMixin as PrototypeMixin
+from dilib.specs import Singleton as Singleton
+from dilib.specs import SingletonDict as SingletonDict
+from dilib.specs import SingletonList as SingletonList
+from dilib.specs import SingletonMixin as SingletonMixin
+from dilib.specs import SingletonTuple as SingletonTuple
+from dilib.specs import Spec as Spec
+from dilib.specs import SpecID as SpecID
