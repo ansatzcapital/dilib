a8c75ffb129ef6e69ba7c06a0874c5b7771f047e
yanovs <42386943+yanovs@users.noreply.github.com>
2025-01-25 17:02:01 -0500

    Switch to pydata theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 
 html_theme_options = {
