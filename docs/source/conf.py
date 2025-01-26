5fd38b6e8ab171a2c9decdfbdc2b2cbf98a39f76
yanovs <42386943+yanovs@users.noreply.github.com>
2025-01-26 07:27:10 -0500

    Set version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,36 +3,41 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+import dilib.version
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'dilib'
-copyright = '2025, author'
-author = 'author'
+project = "dilib"
+copyright = "2025, author"  # noqa: A001
+author = "author"
+
+version = dilib.version.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon',
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = [
-    '**/dilib.rst',
-    '**/modules.rst',
+    "**/dilib.rst",
+    "**/modules.rst",
 ]
 
-language = 'en'
+language = "en"
 
 # See https://www.sphinx-doc.org/en/master/usage/markdown.html
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
 }
 
 # See https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#auto-generated-header-anchors
@@ -41,11 +46,12 @@ myst_heading_anchors = 3
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 
 html_theme_options = {
-    'collapse_navigation': True,  # Collapses navigation items
-    'navigation_depth': 2,        # Adjusts depth of sidebar links
+    # Collapses navigation items
+    "collapse_navigation": True,
+    # Adjusts depth of sidebar links
+    "navigation_depth": 2,
 }
-
